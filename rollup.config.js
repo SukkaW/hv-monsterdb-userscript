18bdcea123cc4ef2bea943f5b87164925b0fd690
SukkaW <isukkaw@gmail.com>
2021-11-07 04:20:35 +0800

    chore(build): fix es5 version userscript meta

--- rollup.config.js
+++ rollup.config.js
@@ -115,6 +115,7 @@ export default [{
     metablock({
       ...userScriptMetaBlockConfig,
       override: {
+        ...userScriptMetaBlockConfig.override,
         grant: [
           'unsafeWindow',
           'GM.getValue',
