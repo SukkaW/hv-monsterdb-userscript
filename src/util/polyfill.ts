543dfc1595a6a9fdaad000aa767df949cef5a7dd
SukkaW <isukkaw@gmail.com>
2021-12-04 00:47:30 +0800

    chore(polyfill): remove fetch polyfill
    Browsers without proper fetch support has been dropped

--- src/util/polyfill.ts
+++ src/util/polyfill.ts
@@ -31,66 +31,8 @@ export function polyfill(): void {
     clearTimeout(id);
   });
 
-  // A bare minimal fetch polyfill
-  if (typeof fetch !== 'function') {
-    globalThis.fetch = function (url, options = {}) {
-      return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        const keys: string[] = [];
-        const all: [string, string][] = [];
-        const headers: { [key: string]: string } = {};
-
-        const response = () => ({
-          // eslint-disable-next-line no-bitwise
-          ok: (request.status / 100 | 0) === 2, // 200-299
-          statusText: request.statusText,
-          status: request.status,
-          url: request.responseURL,
-          text: () => Promise.resolve(request.responseText),
-          json: () => Promise.resolve(request.responseText).then(JSON.parse),
-          blob: () => Promise.resolve(new Blob([request.response])),
-          clone: response,
-          headers: {
-            keys: () => keys,
-            entries: () => all,
-            get: (n: string) => headers[n.toLowerCase()],
-            has: (n: string) => n.toLowerCase() in headers
-          }
-        });
-
-        request.open(options?.method || 'get', typeof url === 'string' ? url : url.url, true);
-
-        request.onload = () => {
-          request.getAllResponseHeaders().replace(/^(.*?):[^\S\n]*([\s\S]*?)$/gm, (_, key, value) => {
-            keys.push(key = key.toLowerCase());
-            all.push([key, value]);
-            headers[key] = headers[key] ? `${headers[key]},${value}` : value;
-            return _;
-          });
-          resolve(response() as unknown as Response);
-        };
-
-        request.onerror = reject;
-
-        request.withCredentials = options?.credentials === 'include';
-
-        for (const i in options?.headers) {
-          if (Object.prototype.hasOwnProperty.call(options?.headers, i)) {
-            const headerContent = options?.headers[i as keyof typeof options.headers];
-            if (typeof headerContent === 'string') {
-              request.setRequestHeader(i, headerContent);
-            }
-          }
-        }
-
-        // @ts-expect-error It is only a fetch polyfill
-        request.send(options?.body || null);
-      });
-    };
-  }
-
   // GM v4 API Polyfill
-  if (typeof GM === 'undefined' ?? GM == null) {
+  if (typeof GM === 'undefined' || GM == null) {
     globalThis.GM = {
       log: console.log.bind(console), // For Pale Moon compatibility
       info: {
@@ -99,8 +41,8 @@ export function polyfill(): void {
           resources: globalThis.GM_info.script.resources as Record<string, { name: string, url: string, mimetype: string }>,
           // @ts-expect-error ViolentMonkey uses runAt while TamperMonkey uses "run-at"
           runAt: (globalThis.GM_info.script.runAt ?? globalThis.GM_info.script['run-at']).replace('document-', '') as 'end' | 'start' | 'idle',
-          // In case some userscript really need it. Always return a random uuid
-          uuid: createUuid(),
+          // In case some userscript really need it. Always return a static uuid
+          uuid: '5be3dfb9-dcd3-433c-8a28-f3996861b7c6',
           version: globalThis.GM_info.script.version
         },
         scriptMetaStr: globalThis.GM_info.scriptMetaStr,
@@ -157,7 +99,3 @@ function createNotification(options: {
     }
   });
 }
-
-// @ts-expect-error It is magic!
-// eslint-disable-next-line no-mixed-operators, no-bitwise
-function createUuid(a?: string): string { return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, createUuid); }
