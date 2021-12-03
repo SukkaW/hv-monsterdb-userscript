0a160425300af51996afd6343fff25069d58e58f
SukkaW <isukkaw@gmail.com>
2021-12-04 07:48:00 +0800

    chore: make typescript happy

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,6 +2,16 @@ import { ISettings } from './types';
 
 declare global {
   const SETTINGS: ISettings;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const __buildMatrix__: 'modern' | 'legacy';
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  function GM_notification(
+    text: string,
+    title: string,
+    image?: string,
+    onClick?: () => void
+  ): void;
 }
 
 export {};
